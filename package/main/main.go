3a2202d2f154ae4fc13d80887d64f123911191c3
chrifost <chris.foster78@gmail.com>
2017-06-16 19:51:02 -0400

    Initial commit of package template

---
+++ package/main/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import "github.com/chrifost/udemy-learn-golang/package/stringutil"
+
+func main() {
+	fmt.Println(stringutil.MyName)
+}
---
+++ package/stringutil/name.go
@@ -0,0 +1,4 @@
+package stringutil
+
+// Make sure is capital so its exported
+var MyName = "Chris"
